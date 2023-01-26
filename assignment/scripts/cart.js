a0d5cd2573b74418a5f5f37772b8c787655abf8c
Sam Gossie <sjg3609@gmail.com>
2023-01-25 20:53:39 -0600

    Added functions and tested them in the console.log

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,46 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+let basket = [];
+
+function addItem(item) {
+    basket.push(item);
+    console.log(basket);
+    return true;    
+}
+
+let result = addItem('ketchup');
+console.log(addItem('chocolate'));
+
+
+function listItems() {
+    for(let item of basket) {
+        console.log(item)
+    }
+}
+
+function empty() {
+    basket = '';
+}
+
+listItems();
+console.log(`Basket is ${basket}`);
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+empty();
+console.log(`Basket is ${basket}`);
+
+// STRETCH GOALS
+
+const maxItems = 5;
+
+function isFull() {
+    if (maxItems < 5) {
+        return false;
+    } else if (maxItems >=5) {
+        return false;
+    }
+
+}
